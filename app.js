06eeb3409249043eb812d029249e80bbba2d88c0
Samet Aylak <sametaylak29@gmail.com>
2016-01-26 18:33:39 +0200

    test

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-io.on('connection', function(socket){
-  console.log('a user connected');
-});
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+// app.js
+var express = require('express');  
+var app = express();  
+var server = require('http').createServer(app);  
+var io = require('socket.io')(server);
+
+server.listen(4200);  
