45a0a8d4e32bb4d6aa93c53d55acd38799140588
Samet Aylak <sametaylak29@gmail.com>
2016-01-26 18:34:05 +0200

    test

--- app.js
+++ app.js
@@ -4,4 +4,6 @@ var app = express();
 var server = require('http').createServer(app);  
 var io = require('socket.io')(server);
 
-server.listen(4200);  
+server.listen(4200, function() {
+	console.log("server dinleniyor");
+});  
