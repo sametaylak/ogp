35add8a7f69974da5709fe78d047da65ad473348
Samet Aylak <sametaylak29@gmail.com>
2016-01-26 18:04:48 +0200

    test

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
+var io = require("socket.io").listen(8100);
 
-var io = require('socket.io')(server);
-var port = process.env.PORT || 3000;
-
-socket.on('connect', function () { console.log("socket connected"); });
+io.sockets.on("connection",function(socket){
+    console.log("Server-Client Connected!");
+});
